a517fd0abd7d4b40db5d297e073b27b3542d75d4
Sushanta65 <sushanta9119@gmail.com>
2025-03-05 23:10:09 +0600

    Resolved Router Problem

--- src/routers/mainRouter.jsx
+++ src/routers/mainRouter.jsx
@@ -1,9 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
+import Page404 from "../pages/Page404/Page404";
 
 const mainRouter = createBrowserRouter([
     {
         path: '/',
-        element: <h2>Home Page</h2>
+        element: <Home />
+    },
+    {
+        path: '/about',
+        element: <About/>
+    },
+    {
+        path: '*',
+        element: <Page404 />
     }
 ])
 
