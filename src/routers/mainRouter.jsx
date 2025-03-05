1439a20f56864b2da5832e75a5c6f4ded45c80ee
Sushanta65 <sushanta9119@gmail.com>
2025-03-05 23:17:04 +0600

    Page Layout Setup Done

--- src/routers/mainRouter.jsx
+++ src/routers/mainRouter.jsx
@@ -2,20 +2,29 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import About from "../pages/About/About";
 import Page404 from "../pages/Page404/Page404";
+import MainLayout from "../Layouts/MainLayout/MainLayout";
 
 const mainRouter = createBrowserRouter([
     {
         path: '/',
-        element: <Home />
-    },
-    {
-        path: '/about',
-        element: <About/>
+        element: <MainLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Home/>
+            },
+            {
+                path: '/about',
+                element: <About/>
+            }
+            
+        ]
     },
     {
         path: '*',
         element: <Page404 />
     }
+    
 ])
 
 export default mainRouter;
