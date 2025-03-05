e20f2e5d375af634c989ea0d95b06cee4457d703
Sushanta65 <sushanta9119@gmail.com>
2025-03-05 23:02:06 +0600

    Router Setup Done

---
+++ src/routers/mainRouter.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const mainRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <h2>Home Page</h2>
+    }
+])
+
+export default mainRouter;
