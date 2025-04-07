b1cc32e4779efbb9c87353c0f0a24f012abe241d
Sushanta65 <sushanta9119@gmail.com>
2025-04-07 23:59:24 +0600

    resolved a problem

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
+        <div className='w-full bg-blue-900 text-white p-5'>
             <h1>Navbar</h1>
         </div>
     );
 };
-
 export default Navbar;
